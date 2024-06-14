eb1dcc70f3d5f7f3b28c13d2bd08dcecfb677494
kittendust21 <nadirhadd22@gmail.com>
2024-06-14 12:25:50 +0700

    update login

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 3001;
@@ -9,19 +8,11 @@ const port = 3001;
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: 'localhost',
     user: 'username',
     password: 'password',
     database: 'dokter'
-});
-
-db.connect((err) => {
-    if (err) {
-        console.log('Error connecting to database:', err);
-        return;
-    }
-    console.log('Connected to database');
 });
 
 app.post('/register', async (req, res) => {
@@ -31,7 +22,7 @@ app.post('/register', async (req, res) => {
         const hashPw = await bcrypt.hash(password, 12);
 
         const sql = 'INSERT INTO users (username, password) VALUES(?, ?)';
-        const [result] = await pool.query(sql, [username, hashPw]);
+        await db.execute(sql, [username, hashPw]);
 
         res.status(200).json({ message: 'success' });
     } catch (error) {
@@ -44,74 +35,75 @@ app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     try {
-        const sql = 'SELECT * FROM users WHERE username = ?';
-        const [rows] = await pool.query(sql, [username]);
+        const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
+        const [rows] = await db.execute(sql, [username, password]);
 
         if (rows.length === 0) {
             return res.status(401).json({ message: 'invalid' });
         }
 
         const user = rows[0];
-        const passwordMatch = await bcrypt.compare(password, user.password);
 
-        if (passwordMatch) {
-            res.status(200).json({message: 'success', user});
-        } else {
-            res.status(401).json({ message: 'invalid' });
-        }
+        const userData = {
+            username: user.username,
+            name: user.name
+        };
+
+        res.status(200).json({ message: 'success', user });
+
     } catch (error) {
         console.log('failed', error);
-        res.status(500).json({ message: 'error' });
+        res.status(500).json({ message: 'error', error });
     }
 });
 
-app.get('/login', (req, res) => {
-    db.query('SELECT * FROM users', (err, results) => {
-        if (err){
-            console.log('Error fetching profiles:', err);
-            res.status(500).send('Server Error');
-            return;
-        }
-        res.json(results);
-    });
+app.post('/posts', async(req, res) => {
+    const { name, content } = req.body;
+
+    try {
+        const sql = 'INSERT INTO posts (username, content) VALUES (?, ?)';
+        await db.query(sql, [name, content]);
+
+        res.status(200).json({message: 'Post created'});
+    } catch (error) {
+        console.error('Post error', error);
+        res.status(500).send({message: 'server error'});
+    }
 });
 
-app.get('/profiles', (req, res) => {
-    db.query('SELECT * FROM profile', (err, results) => {
-        if (err){
-            console.log('Error fetching profiles:', err);
-            res.status(500).send('Server Error');
-            return;
-        }
+app.get('/profiles', async (req, res) => {
+    try{
+        const [results] = await db.execute('SELECT * FROM profile');
         res.json(results);
-    });
+    } catch (err) {
+        console.error('Error');
+        res.status(500).send('Server Error');
+    }
 });
 
-app.get('/pasien', (req, res) => {
-    db.query('SELECT * FROM pasien', (err, results) => {
-        if (err){
-            console.log('Error fetching profiles:', err);
-            res.status(500).send('Server Error');
-            return;
-        }
+app.get('/pasien', async (req, res) => {
+    try {
+        const [results] = await db.execute('SELECT * FROM pasien');
         res.json(results);
-    });
+    } catch (err) {
+        console.error('Error');
+        res.status(500).send('Server Error');
+    }
 });
 
-app.post('/pasien', (req, res) => {
+app.post('/pasien', async (req, res) => {
     const { NIK, Nama_Pasien, Jenis_Pasien, Nomor_HP, Dokter, Poli, Tanggal_Periksa } = req.body;
 
     const sql = 'INSERT INTO pasien (NIK, `Nama Pasien`, `Jenis Pasien`, `Nomor HP`, `Dokter`, `Poli`, `Tanggal Periksa`) VALUES (?, ?, ?, ?, ?, ?, ?)';
     const values = [NIK, Nama_Pasien, Jenis_Pasien, Nomor_HP, Dokter, Poli, Tanggal_Periksa];
 
-    db.query(sql, values, (err, result) => {
-        if(err) {
-            console.error('Gagal memproses booking', err);
-            res.status(500).send('Server Error');
-            return;
-        }
+    try {
+        await db.execute(sql, values);
         res.send('Booking Berhasil!');
-    });
+    } catch (err) {
+        console.error('Gagal Booking', err);
+        res.status(500).send('Server Error');
+    }
 });
 
 
